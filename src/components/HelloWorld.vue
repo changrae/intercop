5a8b79e3850752bfd7733a224572a8cb794af7dc
root <crjang75@gmail.com>
2023-02-18 02:31:21 +0000

    deploy test

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>ICT Smart Controll Devices</h2>
+    <ul>
+      <li>
+        <a
+          href="https://#"
+          target="_blank"
+        >
+          🐮축사환경제어기
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://#"
+          target="_blank"
+        >
+         🐷돈사환경제어기
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://#"
+          target="_blank"
+        >
+          🐔양계환경제어기
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://twitter.com/goodkn"
+          target="_blank"
+        >
+          🏙️도시형환경제어기
+        </a>
+      </li>
+      <br>
+    </ul>
+    <h2>반도체장비 전문제조 업체</h2>
+    <ul>
+      <li>
+        <a
+          href="http://#/"
+          target="_blank"
+        >
+         Semiconductor Rrobot-lift 
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://#/"
+          target="_blank"
+        >
+          PCB & Artwork
+        </a>
+      </li>
+      <li>
+        <a
+          href="http://#/"
+          target="_blank"
+        >
+          Ventilation System
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://#"
+          target="_blank"
+        >
+         Electronic dev device 
+        </a>
+      </li>
+    </ul>
+<br>
+<ul>
+<li><span>GOODKN. Copyallrights reserved</span></li>
+</ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to GOODKNelectro Company'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+span {
+  font-size: 2.2pt
+}
+</style>
