235f8c5f18d00743a50e8fad744ba2e8865abc72
changrae <crjang75@gmail.com>
2023-02-18 14:13:34 +0900

    bootstrapImport

---
+++ src/components/Window.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- 현재열린 상태표시
+  측창제어 채널리스트
+  -----------------------
+  1번채널: [현재 열린상태표시(ex,75%)]상 버튼,하 버튼
+  2번채널: [현재 열린상태표시()]상 버튼,하 버튼
+  3번채널: [현재 열린상태표시()]상 버튼,하 버튼
+  4번채널: [현재 열린상태표시()]상 버튼,하 버튼
+  5번채널: [현재 열린상태표시()]상 버튼,하 버튼
+  6번채널: [현재 열린상태표시()]상 버튼,하 버튼
+
+  온도값에 따른 자동제어
+  -----------------------
+  현재측정된 온도값 표시
+  측창제어를 위한 온도조절 버튼
+
+  수동/자동 변환(S/w)
+  -----------------------
+  -->
+
+
+
+</template>
+<script></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js" integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous"></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous"></link>
+<style></style>
