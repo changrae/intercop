3cfaa4a8cec83966bd2313375c083baaa895693e
changrae <crjang75@gmail.com>
2023-02-18 14:19:56 +0900

    bootstrap edit

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js'
+import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css'
 
 Vue.config.productionTip = false
 
